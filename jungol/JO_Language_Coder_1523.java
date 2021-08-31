3bbc9b27db04221e0338765c661165f2119ead45
redniche <redniche@naver.com>
2021-09-01 00:59:34 +0900

    Solve problems in jungol
    JUNGOL
     - Number of solved: 8 -> 10

---
+++ jungol/JO_Language_Coder_1523.java
@@ -0,0 +1,52 @@
+package com.ssafy.im;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class JO_Language_Coder_1523 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		if (0 > n || 100 < n) {
+			System.out.print("INPUT ERROR!");
+			return;
+		}
+		switch (m) {
+		case 1:
+			for (int i = 0; i < n; i++) {
+				for (int j = -1; j < i; j++) {
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+			break;
+		case 2:
+			for (int i = n; i >= 0; i--) {
+				for (int j = 0; j < i; j++) {
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+			break;
+		case 3:
+			for (int i = 0; i < n; i++) {
+				for (int j = 1; j < n - i; j++) {
+					System.out.print(" ");
+				}
+
+				for (int k = 0; k < 2 * i + 1; k++) {
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+			break;
+		default:
+			System.out.print("INPUT ERROR!");
+		}
+	}
+
+}
